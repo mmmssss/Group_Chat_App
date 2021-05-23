5c6e98556eab08418f32e7d621f4866fecaeef52
mmmssss <44014471+mmmssss@users.noreply.github.com>
2021-05-23 21:42:32 +0900

    Delete firebase_Aline directory

--- firebase_Aline/Assets/Scripts/ListClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-[System.Serializable]
-public class ListClass
-{
-    public string roomIDPass;
-    public List<Dictionary<string, string>> list;
-}
